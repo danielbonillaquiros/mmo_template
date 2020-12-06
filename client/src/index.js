9e600473b668575d57c02b0ec1a8b8da15c37032
Daniel Bonilla <daniel.bonilla.quiros@gmail.com>
2020-12-06 12:27:57 -0600

    Setup webpack

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,21 @@
-console.log('hi');
-const x = 12;
-console.log(x);
+import 'phaser';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload,
+        create,
+    },
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+    console.log('this is the preload method')
+}
+
+function create() {
+    this.add.text(0, 0, 'hello world');
+}
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,11 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/build/',
+        filename: 'project.bundle.js',
+    },
     module: {
         rules: [
             {
