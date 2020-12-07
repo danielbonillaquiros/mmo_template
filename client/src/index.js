b806bddd957833c643f4d03b7aec0979f3b2160a
Daniel Bonilla <daniel.bonilla.quiros@gmail.com>
2020-12-07 07:46:13 -0600

    Add ESlint

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,20 @@
-import 'phaser';
+import * as Phaser from 'phaser';
+
+function create() {
+  this.add.text(0, 0, 'hello world');
+}
 
 const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: () => {
-            console.log('test');
-        },
-        create,
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: () => {
+      console.log('test');
     },
+    create,
+  },
 };
 
 const game = new Phaser.Game(config);
-
-function create() {
-    this.add.text(0, 0, 'hello world');
-}
+console.log(game);
