ac41250ca6a8fb532c349d3a255c1d1876ce24df
Daniel Bonilla <daniel.bonilla.quiros@gmail.com>
2020-12-21 17:40:19 -0600

    Update SignUp

--- client/src/scenes/SignUpScene.js
+++ client/src/scenes/SignUpScene.js
@@ -1,71 +1,27 @@
-import * as Phaser from 'phaser';
-import UiButton from '../classes/UiButton';
+import CredentialsBaseScene from './CredentialsBaseScene';
 import {
-  postData, createDiv, createLabel, createInputField, createBrElement,
+  postData, createLabel, createInputField, createBrElement,
 } from '../utils/utils';
 
-export default class SignUpScene extends Phaser.Scene {
+export default class SignUpScene extends CredentialsBaseScene {
   constructor() {
     super('SignUp');
   }
 
   create() {
-    // create title text
-    this.titleText = this.add.text(this.scale.width / 2, this.scale.height / 8, 'MMORPG', { fontSize: '64px', fill: '#fff' });
-    this.titleText.setOrigin(0.5);
-
-    this.button1 = new UiButton(
-      this,
-      this.scale.width / 2,
-      this.scale.height * 0.75,
-      'button1',
-      'button2',
-      'Sign Up',
-      this.signUp.bind(this),
-    );
-
-    this.button3 = new UiButton(
-      this,
-      this.scale.width / 2,
-      this.scale.height * 0.90,
-      'button1',
-      'button2',
-      'Back',
-      this.startScene.bind(this, 'Title'),
-    );
-
-    this.createInput();
+    this.createUi('Sign Up', this.signUp.bind(this), 'Back', this.startScene.bind(this, 'Title'));
+    this.createUserNameInput();
   }
 
-  createInput() {
-    this.div = createDiv('input-div');
-    this.loginLabel = createLabel('login', 'Email:', 'form-label');
-    this.loginInput = createInputField('text', 'login', 'login', 'login-input', 'Email Address');
-    this.passwordLabel = createLabel('password', 'Password', 'form-label');
-    this.passwordInput = createInputField('password', 'password', 'password', 'login-input');
+  createUserNameInput() {
     this.userNameLabel = createLabel('username', 'Username:', 'form-label');
     this.userNameInput = createInputField('text', 'username', 'username', 'login-input', 'Username');
 
-    this.div.append(this.loginLabel);
-    this.div.append(createBrElement());
-    this.div.append(this.loginInput);
-    this.div.append(createBrElement());
-    this.div.append(createBrElement());
-    this.div.append(this.passwordLabel);
-    this.div.append(createBrElement());
-    this.div.append(this.passwordInput);
     this.div.append(createBrElement());
     this.div.append(createBrElement());
     this.div.append(this.userNameLabel);
     this.div.append(createBrElement());
     this.div.append(this.userNameInput);
-
-    document.body.appendChild(this.div);
-  }
-
-  startScene(targetScene) {
-    this.div.parentNode.removeChild(this.div);
-    this.scene.start(targetScene);
   }
 
   signUp() {
