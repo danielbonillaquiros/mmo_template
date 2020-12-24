22de486a3c6b40762359fec4276ee6d568f85ef3
Daniel Bonilla <daniel.bonilla.quiros@gmail.com>
2020-12-24 06:56:02 -0600

    Add messages to the dialog window

--- client/src/classes/DialogWindow.js
+++ client/src/classes/DialogWindow.js
@@ -21,10 +21,19 @@ export default class DialogWindow {
     this.windowWidth = 305;
     this.windowHeight = this.scene.scale.height;
 
+    this.messages = [];
+    this.messageCount = 0;
+    this.messagesHeight = 0;
+    this.messageGroup = this.scene.add.group();
+
     this.graphics = this.scene.add.graphics();
     this.graphics.setDepth(2);
     this.createWindow();
     this.makeInteractive();
+
+    setInterval(() => {
+      this.addNewMessage({ name: 'test', message: 'test' });
+    }, 500);
   }
 
   createWindow() {
@@ -62,10 +71,19 @@ export default class DialogWindow {
     if (this.rect) {
       this.rect.setPosition(x + 1, y + 1);
       this.rect.setDisplaySize(rectWidth - 1, rectHeight - 1);
+
+      // update position of dialog container
+      this.dialogContainer.setPosition(x + 1, y + 1);
+      this.dialogContainer.setAlpha(this.textAlpha);
     } else {
       this.rect = this.scene.add.rectangle(x + 1, y + 1, rectWidth - 1, rectHeight - 1);
       if (this.debug) this.rect.setFillStyle(0x6666ff);
       this.rect.setOrigin(0, 0);
+
+      // create dialog container for the chat messages
+      this.dialogContainer = this.scene.add.container(x + 1, y + 1);
+      this.dialogContainer.setDepth(3);
+      this.dialogContainer.setAlpha(this.textAlpha);
     }
   }
 
@@ -97,5 +115,32 @@ export default class DialogWindow {
       this.textAlpha = 0.2;
       this.redrawWindow();
     });
+  }
+
+  addNewMessage(messageObject) {
+    this.messages.push(messageObject);
+
+    const windowDimensions = this.calculateWindowDimensions();
+    const message = `${messageObject.name}: ${messageObject.message}`;
+
+    let messageText = this.messageGroup.getFirstDead();
+    if (!messageText) {
+      messageText = this.scene.add.text(0, this.messagesHeight, message, {
+        fontSize: '18px',
+        fill: '#fff',
+        wordWrap: {
+          width: windowDimensions.rectWidth,
+        },
+      });
+      this.messageGroup.add(messageText);
+    } else {
+      messageText.setText(message);
+      messageText.setY(this.messagesHeight);
+      messageText.setActive(true);
+      messageText.setVisible(true);
+    }
+    this.dialogContainer.add(messageText);
+    this.messageCount += 1;
+    this.messagesHeight += messageText.height;
   }
 }
