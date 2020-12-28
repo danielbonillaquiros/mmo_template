a50affe3437ea2bcee2fd42df6306e3a92c91241
Daniel Bonilla <daniel.bonilla.quiros@gmail.com>
2020-12-28 10:33:01 -0600

    Display inventory window

--- client/src/scenes/UiScene.js
+++ client/src/scenes/UiScene.js
@@ -1,4 +1,5 @@
 import * as Phaser from 'phaser';
+import InventoryWindow from '../classes/InventoryWindow';
 
 export default class UiScene extends Phaser.Scene {
   constructor() {
@@ -8,11 +9,18 @@ export default class UiScene extends Phaser.Scene {
   init() {
     // grab a reference to the game scene
     this.gameScene = this.scene.get('Game');
+    // TODO: set to false
+    this.showInventory = true;
   }
 
   create() {
     this.setupUiElements();
     this.setupEvents();
+
+    // handle game resize
+    this.scale.on('resize', this.resize, this);
+    // resize our game
+    this.resize({ height: this.scale.height, width: this.scale.width });
   }
 
   setupUiElements() {
@@ -20,11 +28,26 @@ export default class UiScene extends Phaser.Scene {
     this.scoreText = this.add.text(35, 8, 'Coins: 0', { fontSize: '16px', fill: '#fff' });
     // create coin icon
     this.coinIcon = this.add.image(15, 15, 'items', 3);
+
+    // create inventory modal
+    this.inventoryWindow = new InventoryWindow(this, {
+      windowWidth: this.scale.width / 2,
+      windowHeight: this.scale.height * 0.8,
+      borderAlpha: 1,
+      windowAlpha: 0.9,
+      debug: false,
+      textAlpha: 1,
+      windowColor: 0x000000,
+    });
   }
 
   setupEvents() {
     this.gameScene.events.on('updateScore', (score) => {
       this.scoreText.setText(`Coins: ${score}`);
     });
+  }
+
+  resize(gameSize) {
+    if (this.inventoryWindow) this.inventoryWindow.resize(gameSize);
   }
 }
